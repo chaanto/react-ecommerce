663dda6c31d16fa46a169e5118bfce7a071da261
chaanto <charlieaprilianto52@gmail.com>
2019-11-05 23:11:19 +0700

    creating new exercise project using react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from './home/homepage-component';
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
