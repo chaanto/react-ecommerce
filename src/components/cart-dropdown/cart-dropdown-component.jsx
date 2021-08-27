1d0765778750b83a5532686501054011b3b46ff7
chaanto <charlieaprilianto52@gmail.com>
2021-08-27 23:12:35 +0700

    adding function for  mapping cart item and create componenet for cart-items, fix data from shop-data

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+
 import CustomButton from '../custom-button/custom-button-component';
+import CartItem from '../cart-item/cart-item-component';
 
 import './cart-dropdown-style.scss';
+import { connect } from 'react-redux';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
-            <CustomButton>CHECKOUT</CustomButton>
+            {
+                cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} 
+                />
+            ))}
         </div>
+        <CustomButton>CHECKOUT</CustomButton>
     </div>
 )
 
+const mapStateToProps = ({cart : { cartItems }}) => ({
+    cartItems
+})
 
-export default CartDropdown;
+
+export default connect(mapStateToProps)(CartDropdown);
